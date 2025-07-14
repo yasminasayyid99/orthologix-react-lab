d6e88b848d610bb329bf82028867e9bb489d4247
yasminasayyid99 <yasminamasters99@gmail.com>
2025-07-14 01:46:46 -0500

    Lab1StaticSite.js

---
+++ Lab1StaticSite.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Lab1StaticSite() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>OrthoLogix: Original Static Website (Lab 1)</h1>
+      <p>
+        This page demonstrates my original Lab 1 HTML/CSS website.
+        <br />
+        <a href="https://yasminasayyid99.github.io/orthologix-demo/" target="_blank" rel="noopener noreferrer">
+          View the Lab 1 Website (GitHub Pages)
+        </a>
+      </p>
+      {/* Optionally paste your HTML code here as JSX for in-app preview */}
+    </div>
+  );
+}
+
+export default Lab1StaticSite;
