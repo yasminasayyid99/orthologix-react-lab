5d028491d312e2c72660b46634bb775c9708c129
yasminasayyid99 <yasminamasters99@gmail.com>
2025-07-14 01:48:14 -0500

    Lab3Demo.js

---
+++ Lab3Demo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Lab3Demo() {
+  return (
+    <div>
+      <h2>Lab 3: OrthoLogix Advanced Surgical Tracker</h2>
+      {/* Put your Lab 3 code/UI here */}
+    </div>
+  );
+}
+export default Lab3Demo;
